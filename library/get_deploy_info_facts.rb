53a052b9639465789ff2a5fd0620c78d07ee628e
Max Albrecht <max.albrecht@zhdk.ch>
2021-02-22 16:48:59 +0100

    dev: split up release notes

--- library/get_deploy_info_facts.rb
+++ library/get_deploy_info_facts.rb
@@ -9,6 +9,7 @@ TAG_PREFIX = ''.freeze # could also be 'v' in some projects ,like `v.1.0.0`
 
 def latest_tag_from_version
   return @_latest_tag_from_version if @_latest_tag_from_version
+  fail unless system('../dev/releasenotes-md-to-yaml.rb') # generate combined YAML file
   release = YAML.load_file('../config/releases.yml')['releases'].first
   @_latest_tag_from_version = \
     "#{TAG_PREFIX}""#{release['version_major']}" \
@@ -28,8 +29,16 @@ def git_commit_id
   @_git_commit_id ||= `cd .. && git log -n 1 --pretty=%H`.strip
 end
 
+def git_commit_id_short
+  @git_commit_id_short ||= `cd .. && git log -n 1 --pretty=%h`.strip
+end
+
+def git_commit_date
+  @_git_commit_date ||= `cd .. && git log -n 1 --pretty=%cI`.strip
+end
+
 def git_commit_messages
-  `cd .. && ./deploy/bin/git-release-notes HEAD`.strip
+  `cd .. && ./dev/git-release-notes origin/stable HEAD`.strip
 end
 
 # main
@@ -40,9 +49,13 @@ begin
     stdout: 'deploy info found.',
     data: {
       git_describe_tag: git_describe_tag,
+      # NOTE: this actually means "is_a_tagged_release", keeping the old alias to not break anything:
       build_is_latest_release: git_describe_tag == latest_tag_from_version,
+      is_a_tagged_release: git_describe_tag == latest_tag_from_version,
       tree_id: git_tree_id,
       commit_id: git_commit_id,
+      commit_id_short: git_commit_id_short,
+      commit_date: git_commit_date,
       commit_messages: git_commit_messages,
       time: Time.now.utc.iso8601
     }
